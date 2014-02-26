19269eed747a83627121fd38518e95ce4a28511d
Han Xiao <han.xiao@cs.helsinki.com>
2014-02-26 19:30:55 +0200

    naive bayes classifier done

--- src/classifier.py
+++ src/classifier.py
@@ -1,6 +1,11 @@
 """
 The document classifier that gives the probability that a document belongs to a given topic
 """
+
+from __future__ import division
+from itertools import groupby
+from collections import Counter
+
 class Classifier ():
     """
     The classifier
@@ -13,14 +18,62 @@ class Classifier ():
 
 class NBClassifier (Classifier):
     
-    def train (self, rows):
+    def train (self, texts, rare_threshold = 1):
         """
         train the Naive Bayes model
         
-        rows: the list of (document, label) 
+        texts: the list of (document, label) 
+        rare_threshold: what is the minimum frequency of words that we would not consider it as rare
         
         Return: None
+
+        
         """
+
+        #compute the prob table for classes, class frequencies
+
+        clsFreq = Counter (map (lambda (t, cls): cls, texts))
+
+        pt = {}
+        for cls in clsFreq.keys ():
+            pt [cls] = clsFreq [cls] / sum(clsFreq.values ())
+
+        self.classes = pt.keys () #the classes
+
+        #the vocabulary containing all the words sorted by alphabetical order
+        vocabulary = sorted(list(set([w for words,cls in texts for w in words])) + ['__RARE__'])
+
+        #compute the CPD
+        #group texts by class
+        textsGroupedByCls = groupby (sorted (texts, key = lambda tpl: tpl [1]), lambda tpl: tpl [1])
+
+        cpd = {}
+
+        #for each class
+        for cls, listOfTexts in textsGroupedByCls:
+            cpd [cls] = {}
+            
+            #count the frequency of each word
+            wordFreqRaw = Counter([w for ts, cls in listOfTexts for w in ts])
+
+            #filter out those whose frequency < rare_threshold 
+            #and aggragate those rare words' frequency
+            wordFreq = Counter ()
+            for w in wordFreqRaw:
+                    
+                if wordFreqRaw [w] < rare_threshold:
+                    wordFreq ["__RARE__"] += wordFreqRaw [w]
+                else:
+                    wordFreq [w] = wordFreqRaw [w]
+
+            totalCount = sum(wordFreq.values ())
+            
+            #for each word in the vocabulary, calcualte the relative frequency (with smoothing)
+            for w in vocabulary:
+                cpd [cls][w] = (wordFreq [w] + 1) / (totalCount + len (vocabulary))
+                
+        self.pt = pt
+        self.cpd = cpd
 
     def predict (self, words):
         """
@@ -28,6 +81,19 @@ class NBClassifier (Classifier):
         
         words: list of word (str)
 
-        Return: the probability the document is positive
+        Return: list of (class, the probability) in ascending order by the probability
         """
+        probs = {}
+        total = 0
+        for cls in self.classes:
+            #if rare words are seen, use the __RARE__ item
+            probs [cls] = reduce (lambda acc, word: acc * self.cpd [cls] [word if self.cpd [cls].has_key (word) else '__RARE__'] , words, self.pt [cls])
+            total += probs [cls]
+            
+        #normalization
+        for cls in self.classes:
+            probs [cls] /= total
+            
+        return sorted(probs.items (), key=lambda p: p [1], reverse = True)
+        
         
---
+++ test/classifier.py
@@ -0,0 +1,45 @@
+import setting
+import unittest
+
+from src.classifier import NBClassifier
+from src.util import read_traindata
+
+class NBClassifierTest (unittest.TestCase):
+    def setUp (self):
+        """
+        first train the model
+        """
+        self.classifier = NBClassifier ()
+        traindata = read_traindata ('data/train.txt')
+        self.classifier.train (traindata)
+
+    def test_prediction1 (self):
+        """
+        test prediction for sample 1
+        """
+        sample = ['FREE', 'online', 'conference', '!!!']
+        prediction = self.classifier.predict (sample)
+
+        self.assertEqual (prediction, [(u'spam', 0.8371836806110771), (u'ham', 0.16281631938892294)])
+
+    def test_prediction2 (self):
+        """
+        test prediction for sample 2
+        """
+        sample = ['conference', 'registration', 'results', 'conference', 'online']
+        prediction = self.classifier.predict (sample)
+        
+        self.assertEqual (prediction, [(u'ham', 0.9683852022829362), (u'spam', 0.031614797717063825)])
+
+    def test_unknown_words (self):
+        """
+        test prediction with unknown words
+        """
+        sample = ['conference', 'registration', 'results', 'conference', 'online', '(&^*^*&^*']
+        prediction = self.classifier.predict (sample)
+        
+        self.assertEqual (prediction, [(u'ham', 0.9698452719061822), (u'spam', 0.030154728093817886)])
+
+
+if __name__ == "__main__":
+    unittest.main ()
