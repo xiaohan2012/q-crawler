911a96b052095f438fd7ba270aeae5dd42a8fdbe
Han Xiao <han.xiao@cs.helsinki.com>
2014-02-28 20:35:35 +0200

    modification on how link surrounding words aar retrieved

--- src/scraper.py
+++ src/scraper.py
@@ -1,53 +1,70 @@
 """
 The scraper that scrapes (text, surrounding text) from webpages
 """
-import re, string
+import re, string, math
 from itertools import chain
-from pyquery import PyQuery
+from pyquery import PyQuery as pq
 
-def recursive_contents (node):
+def strip_all_tags_except (doc, tag_names):
     """
-    recursively get the contents of a node
-    
-    Example: 
-    p
-     span
-     span
-         #some-text
-         a
-     div
-        span
-    should return something like:
-    [p span #some-text a div span]
+    strip all tags in the doc except those included in `tag_names`
 
+    return the 'sanitized' html string
     """
-    contents = []
-    node = PyQuery(node)
-    if len(node.children ()) == 0: #no children
-        return [node [0]]
-    else: #has children
-        for n in node.contents ():
-            if isinstance(n, str):
-                contents.append (n)
-            else:
-                contents.extend (recursive_contents (n))
-    return contents
+    def aux (contents):
+        """
+        auxiliary funciton:
+        strip all the contents of tags except thos included in 'tag_names'
+        """
+        result = []
+        for content in contents:
+            if isinstance(content, str):
+                result.append (content)
+            elif content.tag in tag_names: #is string or the tag we want
+                result.append (pq (content).outerHtml ())
+            elif content is not None:
+                result.extend(aux (pq(content).contents ()))
+
+        return result
+        
+    return ' '.join(aux (pq(doc).contents ()))
+
+def line2words (line):
+    """
+    remove redundent spaces and punctuations
+    all words to lower case :-(
+
+    then turn the line to words
+    """
+    return map(lambda w: w.lower (), re.sub ('[\t\n%s]+' %string.punctuation, ' ', line).split ())
+
+
+def html2words (html):
+    """
+    convert one piece of html to words and tags
+    """
+    sequence = []
+    #map the text to words
+    for node in pq(html).contents ():
+
+        if isinstance(node, str):
+            sequence.extend (line2words (node))
+        else:
+            sequence.append (node)
+
+    return sequence
     
+
 def scrape_url (doc):
     """
     Given the document, return the (url, surrounding words) pairs
     """
     sufficient_word_number = 25
-    def line2words (line):
-        """
-        remove redundent spaces and punctuations
-        all words to lower case :-(
-
-        then turn the line to words
-        """
-        return map(lambda w: w.lower (), re.sub ('[\t\n%s]+' %string.punctuation, ' ', line).split ())
-
-    def get_link_words (link):
+    
+    striped_html = strip_all_tags_except (doc, ['a']) 
+    sequence = html2words (striped_html) #the word, word, link, word... sequence
+   
+    def get_surrouding_words (linkelem):
         """
         The words representing the link
         
@@ -55,53 +72,37 @@ def scrape_url (doc):
         If the word count in title and link text > sufficient_word_number, then using the words in title and link text is ok
         Else, search from siblings(then parents) for more text. If there is no more text, forget about it
         """
-        link = PyQuery (link)
+        link = pq (linkelem)
 
-        title = line2words(link.attr("title"))
+        title = link.attr("title") != None and line2words(link.attr("title")) or []
 
         link_text = line2words(link.text())
 
-        if len(title) + len (link_text) > sufficient_word_number:
+        get_words = lambda seq: filter (lambda sth: isinstance (sth, str), seq)
+        
+        remaining_count = sufficient_word_number - len(title) - len (link_text)
+ 
+        if remaining_count <= 0:
             return title + link_text
         else:
-            parent = PyQuery(link).parent ()
-            while True:
-                parent_words = line2words(parent.text ())
-                if len (parent_words) < (sufficient_word_number - len(link_text) - len (title)): #should substract the link text part
-                    #loop until the parent can cover the sufficient word number
-
-                    if len(parent.parent ()) != 0: # parent exists, we can go further, or that's the best we can get
-                        parent = parent.parent ()
-                        continue
-                        
-                                                               
-                siblings = recursive_contents(parent)#recursively get the contents
-                
-                index = siblings.index (link [0])#where the link is
-                
-                get_text = lambda e: line2words(isinstance (e, str) and str (e) or e.text) #get the text from the dom node, whether text node or element node
-                
-
-                prepending_words  = list(chain.from_iterable (map(get_text, siblings [:index]))) #words within the parent that prepend the link
-
-                appending_words = list(chain.from_iterable (map(get_text, siblings [index+1:]))) #words within the parent that append the link
-                
-                remaining_number = sufficient_word_number - len(link_text) - len (title)
-                
-                #start the greedy search for words
-                half_number = remaining_number / 2
-                if len(prepending_words) >= half_number:
-                    return prepending_words [-half_number: ] + title + link_text + appending_words [:half_number]
-                elif len(prepending_words) < half_number: #to be made up by the appending words
-                    left = remaining_number - len (prepending_words)
-                    return prepending_words + title + link_text + appending_words [: left]
-                else: #to be made up by the prepending words
-                    left = remaining_number - len (apppending_words)
-                    return prepending_words [-left:] + title + link_text + appending_words
-
-        
-    doc = PyQuery (doc)
-    links = map(lambda link: (PyQuery(link).attr ('href'), get_link_words (link)), doc ("a"))
+            
+            index = sequence.index (linkelem)
+            prepending_words = get_words (sequence [:index]); prepending_count = len(prepending_words)
+            appending_words = get_words (sequence [index:]); appending_count = len(appending_words)
+            
+            prepending_half = int(math.floor(float(remaining_count) / 2))
+            appending_half = int(math.ceil(float(remaining_count) / 2))
+            if prepending_count + appending_count >= remaining_count:
+                if appending_count <= appending_half: #prepending words are not enough to fill in half
+                    return prepending_words [-(remaining_count - appending_count):] + title + link_text + appending_words
+                elif prepending_count <= prepending_half: #appending words are not enough to fill in half
+                    return prepending_words + title + link_text + appending_words [:(remaining_count - prepending_count)]
+                else:#both are enough
+                    return prepending_words [-prepending_half:] + title + link_text + appending_words [:appending_half]
+            else: #that is it
+                return prepending_words + title + link_text + appending_words
+ 
+    links = map(lambda link: (pq(link).attr ('href'), get_surrouding_words (link)), filter(lambda ele: not isinstance(ele, str) and ele.tag == 'a', sequence))
     
     #return them
     return links
--- unit_tests/scraper_test.py
+++ unit_tests/scraper_test.py
@@ -1,74 +1,60 @@
 import setting
 import unittest
 
-from src.scraper import scrape_url, recursive_contents
+from src.scraper import scrape_url, strip_all_tags_except, html2words
 
 from pyquery import PyQuery as pq
 
-class RecursiveContentsTest (unittest.TestCase):
-    
-    
-    def test_basic (self):
-        
-        html = """
+class PreprocessingTest (unittest.TestCase):
+    """
+    test striping tags and html2words
+    """
+    def setUp (self):
+        self.html = """
         <p>
           <span>
               Span 1 text
           </span>
           <span>
-              Span 2 text
+              Span 2 !!text
               <a href='http://feng'>link</a>
           </span>
           <div>
-              Div text
+              Div text...
           </div>
           <span>
-              Span 1
+              Span 3.
+              <a href='http://zhang'><sp>sometag</sp>zhanglink</a>
           </span>
         </p>
-        """.replace ('\n', '')
-        
-        
-        node = pq (html)
-
-        contents = recursive_contents (node)
-
-        # check the tags
-        tags = map(lambda node: node.tag, filter(lambda n: not isinstance(n, str), contents)) #get all the tags
-        
-        self.assertEqual(tags, ['span', 'a', 'div', 'span'])
-
-        # check the text 
-        text = map(lambda node: str (node).strip (), filter(lambda n: isinstance(n, str) and len(n.strip ()) > 0, contents)) #non whitespace text
-        self.assertEqual(text, ['Span 2 text'])
-
-    def test_basic1 (self):
-        html = """
-<div>
-    <p>
-      insufficient
-      <a href="http://short_title.com" title="" >really insuffient</a>
-    </p>
-</div>
         """
-        node = pq (html)
 
-        contents = recursive_contents (node)
+    def test_basic (self):
+        remaining = strip_all_tags_except (self.html, ['a'])
+        expected = "Span 1 text Span 2 !!text <a href=\"http://feng\">link</a> Div text... Span 3. <a href=\"http://zhang\"><sp>sometag</sp>zhanglink</a>"
 
-        # check the tags
-        tags = map(lambda node: node.tag, filter(lambda n: not isinstance(n, str), contents)) #get all the tags
+        actual = ' '.join(remaining.split ())
         
-        self.assertEqual(tags, ['a'])
+        self.assertEqual (actual, expected)
 
-        # check the text 
-        text = map(lambda node: str (node).strip (), filter(lambda n: isinstance(n, str) and len(n.strip ()) > 0, contents)) #non whitespace text
-        self.assertEqual(text, ['insufficient'])
-        
+    def test_html2words (self):
+        remaining = html2words(strip_all_tags_except (self.html, ['a']))
+
+        expected = ["span", "1", "text", "span", "2", "text", "<a href=\"http://feng\">link</a>", "div", "text", "span", "3", "<a href=\"http://zhang\"><sp>sometag</sp>zhanglink</a>"]
+
+        for sth,other in zip(remaining, expected):
+            if isinstance(sth, str): #text
+                self.assertEqual (sth, other)
+            else:#html element
+                self.assertEqual (pq(sth).outerHtml(), other)
+
+
+                
 class ScraperTest (unittest.TestCase):
     """
     test for the scraper for URL
     """
-    def test_long_title_linktext (self):
+    def test_long_title (self):
         """
         title and link text exceeds 25
         """
@@ -76,29 +62,30 @@ class ScraperTest (unittest.TestCase):
         links = scrape_url (doc)
         self.assertEqual ([('http://long_title.com', ['if', 'the', 'sentence', 'is', 'more', 'than', 'or', 'equal', 'to', '10', 'words', 'then', 'we', 'assume', 'only', 'this', 'sentence', 'is', 'about', 'the', 'url', 'it', 'is', 'really', 'really', 'long'])], links)
 
-    def test_short_title_linktext_with_text_node (self):
+    def test_short_title (self):
+        """
+        title and link text exceeds 25
+        """
+        doc = open (setting.DIRNAME + '/pages/short_title.html').read ()
+        links = scrape_url (doc)
+        self.assertEqual ([('http://short_title.com', ['domain', 'bla', 'bla', 'bla', 'bla','bla', 'bla', 'bla', 'bla', 'bla', 'it', 'is', 'really', 'short', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla'])], links)
+
+    def test_short_infront (self):
         """
         title and link words number fewer 25 thus need to get words from surrounding. In case the surrounding the text nodes
         """
-        doc = open (setting.DIRNAME + '/pages/short_title_linktext.html').read ()
+        doc = open (setting.DIRNAME + '/pages/short_infront.html').read ()
         links = scrape_url (doc)
-        self.assertEqual ([('http://short_title.com', ['bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'it', 'is', 'really', 'short', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla'])], links)
+        
+        self.assertEqual ([('http://short_title.com', ['example', 'domain', 'bla', 'bla', 'it', 'is', 'really', 'short', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla'])], links)
 
-    def test_short_title_linktext_with_siblings (self):
+    def test_short_behind (self):
         """
         title and link words number fewer 25 thus need to get words from surrounding. In case the surrounding the element nodes
         """
-        doc = open (setting.DIRNAME + '/pages/short_title_linktext_using_span.html').read ()
+        doc = open (setting.DIRNAME + '/pages/short_behind.html').read ()
         links = scrape_url (doc)
-        self.assertEqual ([('http://short_title.com', ['bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'it', 'is', 'really', 'short', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla'])], links)
-
-    def test_short_title_linktext_using_ancestor (self):
-        """
-        title and link words number fewer 25 thus need to get words from surrounding. In case its ancestor
-        """
-        doc = open (setting.DIRNAME + '/pages/short_title_linktext_using_ancestor.html').read ()
-        links = scrape_url (doc)
-        self.assertEqual ([('http://short_title.com', ['bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'it', 'is', 'really', 'short', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla'])], links)
+        self.assertEqual ([('http://short_title.com', ['bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla',  'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'bla', 'it', 'is', 'really', 'short', 'bla', 'bla', 'bla', 'bla'])], links)
 
     def test_insufficient_words (self):
         """
