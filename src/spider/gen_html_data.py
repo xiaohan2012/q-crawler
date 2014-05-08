dcc70b974d19989c183485387daf84b639d71df5
Han Xiao <han.xiao@cs.helsinki.com>
2014-05-08 18:22:06 +0300

    comparison.html added label and title

--- src/spider/gen_html_data.py
+++ src/spider/gen_html_data.py
@@ -1,4 +1,4 @@
-from json import dump
+from json import dumps
 def acc_sum(lis):
     total = 0
     for x in lis:
@@ -9,8 +9,13 @@ if __name__ == "__main__":
     aptc_scores = map(lambda l: float(l.strip().split()[1]), open("data/python/urls-apprentice.txt").readlines())
     bsl_scores = map(lambda l: float(l.strip().split()[1]), open("data/python/urls-baseline.txt").readlines())
     
-    aptc_stat = acc_sum(aptc_scores[::100])
-    bsl_stat = acc_sum(bsl_scores[::100])
+    aptc_stat = acc_sum(aptc_scores)
+    bsl_stat = acc_sum(bsl_scores)
 
-    dump(list(aptc_stat), open('data/python/apprentice_stat.js', "w"))
-    dump(list(bsl_stat), open('data/python/baseline_stat.js', "w"))    
+    with open('apprentice_stat.js', "w") as f:
+        stat_str = dumps(list(aptc_stat)[::250])
+        f.write("var apprentice_data = %s;" %(stat_str))
+
+    with open('baseline_stat.js', "w") as f:
+        stat_str = dumps(list(bsl_stat)[::250])
+        f.write("var baseline_data = %s;" %(stat_str))
